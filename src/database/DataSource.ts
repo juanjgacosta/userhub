3a947ec147bac68c606a18e8c8d7aa0a84d84de8
Juan <juan_jg@hotmail.com>
2026-03-08 08:02:50 -0500

    feat: refactor database connection and update migration commands for consistency

--- src/database/DataSource.ts
+++ src/database/DataSource.ts
@@ -2,20 +2,20 @@ import { DataSource } from 'typeorm'
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  // host: 'localhost',
-  // port: 4001,
-  // username: 'root',
-  // password: 'root',
   database: './database.sqlite',
   synchronize: false,
   logging: false,
   entities: ['./src/modules/users/entities/**.ts'],
   migrations: ['./src/database/migrations/**.ts'],
+  migrationsRun: false,
 })
 
 AppDataSource.initialize()
-  .then((async) => {
+  .then(async () => {
     console.log('Database connection ok')
+
+    await AppDataSource.runMigrations()
+    console.log('Migrations executed successfully')
   })
   .catch((err) => {
     console.error('Database error connection: ', err)
